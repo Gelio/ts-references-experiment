78be989d70f6886285a50db86f7687740c86c429
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-11-10 14:54:28 +0100

    initial commit, add base setup with tslint ordered-imports rule

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+import { component } from 'components/my-component';
+
+import { featureName } from 'features/my-feature';
+
+console.log(useState, featureName, component);
